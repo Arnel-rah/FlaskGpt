8d75c3ff188a2bf5911a4b7034dbd096326fb929
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-09-30 15:34:55 +0300

    feat: add templates directory and render index.html in home route

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>hello</h1>"
+    return render_template('index.html')
 
 if __name__ == __name__:
     app.run(debug=True, host='127.0.0.1', port=5000)
